3a962baabaa2bcfd7f637e97a54362f6d1626ece
neal <neal.blomfield@gmail.com>
2014-04-13 10:53:06 +1200

    Added test for FizzBuzz( 0 )

--- Exercise 1 - FizzBuzz/FizzBuzz.Tests/FizzBuzzGeneratorSpecification.cs
+++ Exercise 1 - FizzBuzz/FizzBuzz.Tests/FizzBuzzGeneratorSpecification.cs
@@ -8,14 +8,26 @@ using FizzBuzz;
 namespace FizzBuzz.Tests
 {
     [TestFixture]
-    public class FizzBuzzGeneratorSpecification
+    public class When_using_FizzBuzzGenerator
     {
         [Test]
-        public void FizzBuzz_Returns_UpperLimit_Items()
+        public void returns_upperLimit_items()
         {
             var fizzbuzz = new FizzBuzzGenerator();
 
             Assert.That(fizzbuzz.FizzBuzz(10).Count(), Is.EqualTo(10), "Expected FizzBuzz( 10 ) to return 10 items"); 
+        }
+
+        [Test]
+        public void returns_no_items_for_zero()
+        {
+            Assert.That(GenerateFizzBuzz(0), Is.Empty, "Expected FizzBuzz( 0 ) to return empty enumeration");
+        }
+
+        private IEnumerable<string> GenerateFizzBuzz(int upperLimit)
+        {
+            var fizzBuzz = new FizzBuzzGenerator();
+            return fizzBuzz.FizzBuzz(upperLimit);
         }
     }
 }
