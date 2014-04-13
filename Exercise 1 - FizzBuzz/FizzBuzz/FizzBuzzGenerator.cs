430bfd4356b623d45b5faaf7248ce6707db6ce7f
neal <neal.blomfield@gmail.com>
2014-04-13 14:13:37 +1200

    Added initial "Buzz" generation

--- Exercise 1 - FizzBuzz/FizzBuzz/FizzBuzzGenerator.cs
+++ Exercise 1 - FizzBuzz/FizzBuzz/FizzBuzzGenerator.cs
@@ -20,6 +20,8 @@ namespace FizzBuzz
             if (value % 3 == 0)
                 return "Fizz";
 
+            if (value % 5 == 0)
+                return "Buzz";
             return value.ToString();
         }
     }
