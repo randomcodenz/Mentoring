3a88938cfbb06bfbbdc7968c20e2bedde6554d57
neal <neal.blomfield@gmail.com>
2014-05-11 13:31:52 +1200

    Creating working binary search

--- Exercise 2 - Binary Search/BinarySearch.Tests/BinarySearchSpecification.cs
+++ Exercise 2 - Binary Search/BinarySearch.Tests/BinarySearchSpecification.cs
@@ -1,9 +1,82 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BinarySearch.Tests
 {
-    [TestFixture]
+    [TestClass]
     public class BinarySearchSpecification
     {
+        [TestMethod]
+        public void Searching_an_empty_array_returns_null()
+        {
+            var emptyArray = new int[0];
+
+            Assert.IsNull(ExecuteFind(emptyArray, 1));
+        }
+
+        [TestMethod]
+        public void Searching_a_one_element_array_containing_the_value_returns_zero()
+        {
+            var array = new int[] { 1 };
+
+            Assert.AreEqual(ExecuteFind(array, 1), 0);
+        }
+
+        [TestMethod]
+        public void Searching_a_one_element_array_not_containing_the_value_returns_null()
+        {
+            var array = new int[] { 2 };
+
+            Assert.IsNull(ExecuteFind(array, 1), null);
+        }
+
+        [TestMethod]
+        public void Searching_a_two_element_array_containing_the_value_returns_the_index()
+        {
+            var array = new int[] { 1,3 };
+
+            Assert.AreEqual(ExecuteFind(array, 3), 1);
+        }
+
+        [TestMethod]
+        public void Searching_a_two_element_array_not_containing_the_value_returns_null()
+        {
+            var array = new int[] { 2,4 };
+
+            Assert.IsNull(ExecuteFind(array, 3), null);
+        }
+
+        [TestMethod]
+        public void Searching_a_five_element_array_not_containing_the_value_returns_null()
+        {
+            var array = new int[] { 1, 2, 6, 7, 9 };
+
+            Assert.IsNull(ExecuteFind(array, 4), null);
+        }
+
+        [TestMethod]
+        public void Correctly_searches_a_five_element_array()
+        {
+            var array = new int[] { 1, 2, 6, 7, 9 };
+
+            Assert.AreEqual(ExecuteFind(array, 1), 0);
+            Assert.AreEqual(ExecuteFind(array, 2), 1);
+            Assert.AreEqual(ExecuteFind(array, 6), 2);
+            Assert.AreEqual(ExecuteFind(array, 7), 3);
+            Assert.AreEqual(ExecuteFind(array, 9), 4);
+        }
+
+        [TestMethod]
+        public void Returns_the_lowest_index_when_array_contains_duplicates()
+        {
+            var array = new int[] { 1, 2, 2, 7, 9 };
+
+            Assert.AreEqual(ExecuteFind(array, 2), 1);
+        }
+
+        private int? ExecuteFind( int[] array, int value )
+        {
+            var searcher = new BinarySearch();
+            return searcher.Find(array, value);
+        }
     }
 }
--- Exercise 2 - Binary Search/BinarySearch/BinarySearch.cs
+++ Exercise 2 - Binary Search/BinarySearch/BinarySearch.cs
@@ -4,7 +4,24 @@
     {
         public int? Find( int[] array, int value )
         {
-            return null;
+            if (array.Length == 0)
+                return null;
+
+            var lower = 0;
+            var upper = array.Length - 1;
+
+            while (lower < upper)
+            {
+                var pivot = (lower + upper) / 2;
+                var pivotValue = array[pivot];
+
+                if (pivotValue < value)
+                    lower = pivot + 1;
+                else
+                    upper = pivot;
+            }
+
+            return lower == upper && array[lower] == value ? lower : (int?)null;
         }
     }
 }
